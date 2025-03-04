7c1d138c68d3d9dcebd3117b8a78e3dd2059bb6f
B00168527 <B00168527@mytudublin.ie>
2025-03-04 10:38:15 +0000

    Add files via upload

---
+++ CryptoHack/Modular Arithmetic/Modular_Arithmetic_1.py
@@ -0,0 +1,11 @@
+# 11 = x mod 6
+
+# 8146798528947 ≡ y mod 17
+
+# Follwoing the formula "a = b mod m" then "a mod m = b"
+
+val1 = 11 % 6
+print("val1 = " + str(val1))
+
+val2 = 8146798528947 % 17
+print("val2 = " + str(val2))
